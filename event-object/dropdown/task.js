9e00431c2810d2bddd2936fac62417a9908c4331
Diana Verevkina <diana_verevkina@mail.ru>
2023-03-30 23:56:49 +0300

    updated dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -2,10 +2,10 @@ let dropdownChoice = document.querySelector('.dropdown__value');
 let dropdownList = document.querySelector('.dropdown__list');
 let dropdownItems = [...document.querySelectorAll('.dropdown__item')];
 
+
 dropdownChoice.addEventListener('click', function() {
   dropdownList.classList.toggle('dropdown__list_active');
 })
-
 
 dropdownItems.forEach(item => {
   item.addEventListener('click', function(event) {
