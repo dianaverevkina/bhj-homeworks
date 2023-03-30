f4186e0f4b2130d1f049b2a20b044c26a121de44
Diana Verevkina <diana_verevkina@mail.ru>
2023-03-30 23:57:52 +0300

    keysolo updated

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -4,6 +4,7 @@ class Game {
     this.wordElement = container.querySelector('.word');
     this.winsElement = container.querySelector('.status__wins');
     this.lossElement = container.querySelector('.status__loss');
+    this.timer = container.querySelector('.timer__seconds');
 
     this.reset();
 
@@ -18,6 +19,9 @@ class Game {
 
   registerEvents() {
     document.addEventListener('keydown', (event) => {
+      if (this.firstSymbol === event.key) {
+        this.startTimer();
+      } 
       if (this.currentSymbol.textContent === event.key) {
         this.success();
       } else {
@@ -26,7 +30,14 @@ class Game {
     });
   }
 
+  startTimer() {
+    this.timerId = setInterval(()=> {
+      if (--this.timer.textContent === 0) this.fail();
+    }, 1000);
+  }
+
   success() {
+    
     if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
@@ -38,14 +49,18 @@ class Game {
 
     if (++this.winsElement.textContent === 10) {
       alert('Победа!');
+      clearInterval(this.timerId);
       this.reset();
     }
+    
+    clearInterval(this.timerId);  
     this.setNewWord();
   }
 
   fail() {
-    if (++this.lossElement.textContent === 5) {
+    if (++this.lossElement.textContent === 5 || this.timer.textContent === 0) {
       alert('Вы проиграли!');
+      clearInterval(this.timerId);
       this.reset();
     }
     this.setNewWord();
@@ -83,8 +98,10 @@ class Game {
           `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
       )
       .join('');
+      
     this.wordElement.innerHTML = html;
-
+    this.firstSymbol = word[0];
+    this.timer.textContent = word.length;
     this.currentSymbol = this.wordElement.querySelector('.symbol_current');
   }
 }
