cce15c6fee4979fba0ecc725ec68224938b225d6
Diana Verevkina <diana_verevkina@mail.ru>
2023-03-31 21:38:29 +0300

    updated menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,20 +1,51 @@
+let mainMenu = document.querySelector('.menu_main')
+let mainMenuLinks = mainMenu.querySelectorAll('.menu__link');
 
-let menuElement = document.querySelector('.menu');
-let menuLinks = Array.from(menuElement.querySelectorAll('.menu__link'));
-let subMenus = Array.from(menuElement.querySelectorAll('.menu_sub'));
-console.log(menuLinks);
+function clickMainMenuLinks(event) {
+  let link = event.target;
+  let item = link.closest('.menu__item');
+  let subMenu = item.querySelector('.menu_sub');
+  // debugger;
+  if (!subMenu) {
+    return;
+  }
+  if (subMenu.classList.contains('menu_active')) {       
+    subMenu.classList.remove('menu_active');
+    event.preventDefault();
+    return;
+  }
+  
+  let subMenus = [...mainMenu.querySelectorAll('.menu_sub')];
 
-menuLinks.forEach( link => {
-    link.onclick = () => {
-      let subMenu = link.nextElementSibling;
-      if (!link.nextElementSibling) return;
-      if (subMenu.classList.contains('menu_active')) {
-        subMenu.classList.remove('menu_active');
-        return;
-      }
-      subMenus.forEach(subMenu => subMenu.classList.remove('menu_active'));
-      subMenu.classList.add('menu_active');
-      return false; 
-    }
-})
+  subMenus.forEach(subMenu => subMenu.classList.remove('menu_active'))
+  subMenu.classList.add('menu_active');
+  event.preventDefault();
+}
 
+mainMenuLinks.forEach(link => link.addEventListener('click', clickMainMenuLinks));
+
+let addMenu = document.querySelector('.menu_add');
+let addMenuLinks = addMenu.querySelectorAll('.menu__link');
+
+function clickAddMenuLinks(event) {
+  let link = event.target;
+  let item = link.closest('.menu__item');
+  let subMenu = item.querySelector('.menu_sub');
+  // debugger;
+  if (!subMenu) {
+    return;
+  }
+  if (subMenu.classList.contains('menu_active')) {       
+    subMenu.classList.remove('menu_active');
+    event.preventDefault();
+    return;
+  }
+  
+  let subMenus = [...addMenu.querySelectorAll('.menu_sub')];
+
+  subMenus.forEach(subMenu => subMenu.classList.remove('menu_active'))
+  subMenu.classList.add('menu_active');
+  event.preventDefault();
+}
+
+addMenuLinks.forEach(link => link.addEventListener('click', clickAddMenuLinks));
