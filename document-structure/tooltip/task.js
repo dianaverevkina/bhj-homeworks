c10e5e970922729217fa80f20913cadda0f35397
Diana Verevkina <diana_verevkina@mail.ru>
2023-04-28 14:35:55 +0300

    updated tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,33 +1,62 @@
 const hasTooltips = document.querySelectorAll('.has-tooltip');
 
+//Создать элемент подсказки
 function createTooltipElement(el) {
   const tooltip = document.createElement('div');
   tooltip.classList.add('tooltip');
   tooltip.innerHTML = el.title;
-  el.after(tooltip);
+  tooltip.dataset.position = 'bottom';
 
-  let coords = el.getBoundingClientRect();
-  
-  if (coords.top < 30) {
-  tooltip.style.left = coords.left + 'px';
-  tooltip.style.top = coords.top + el.offsetHeight + 'px';
-  } else {
-    tooltip.style.left = coords.left + 'px';
-    tooltip.style.top = coords.top +'px';
-    tooltip.dataset.position = 'top';
-  }
+  el.append( tooltip );
+  positionTooltip(el, tooltip);
 
   return tooltip;
 }
 
-function showTooltip(e) {
-  const tooltips = document.querySelectorAll('.tooltip');
-  tooltips.forEach(tip => tip.remove());
 
-  const tooltip = createTooltipElement(this);
-  
-  tooltip.classList.add('tooltip_active');
-  e.preventDefault();
+//Позицонировать подсказку
+function positionTooltip(el, tooltip) {
+  let { top, left, right } = el.getBoundingClientRect();
+
+  if ( tooltip.dataset.position === 'bottom' ) {
+    tooltip.style.left = left + 'px';
+    tooltip.style.top = top + el.offsetHeight + 'px';
+  }
+
+  if ( tooltip.dataset.position === 'top' ) {
+    tooltip.style.left = left + 'px';
+    tooltip.style.top = top +'px';
+    tooltip.style.transform = 'translateY(-100%)';
+  }
+
+  if ( tooltip.dataset.position === 'right' ) {
+    tooltip.style.left = right + 'px';
+    tooltip.style.top = top  + 'px';
+  }
+
+  if ( tooltip.dataset.position === 'left' ) {
+    tooltip.style.left = left + 'px';
+    tooltip.style.top = top + 'px';
+    tooltip.style.transform = 'translateX(-100%)';
+  }
 }
 
-hasTooltips.forEach(hasTooltip => hasTooltip.addEventListener('click', showTooltip));
+//Открыть подсказку
+function showTooltip(e) {
+  e.preventDefault();
+
+  const openedTooltip = this.querySelector('.tooltip');
+
+  if ( openedTooltip ) {
+    openedTooltip.remove();
+    return;
+  }
+
+  const tooltips = document.querySelectorAll( '.tooltip' );
+  tooltips.forEach( tip => tip.remove() );
+
+  const tooltip = createTooltipElement(this);
+  tooltip.classList.add( 'tooltip_active' );
+}
+
+hasTooltips.forEach(hasTooltip => hasTooltip.addEventListener( 'click', showTooltip ));
